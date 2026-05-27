40d34760dd1151a1f399bf0be76cff04e4f734d0
Nanasi <this.saif.email@gmail.com>
2026-05-27 16:56:19 -0400

    feat(cpp): declare async JSI bindings for background worker thread execution

--- cpp/ShaheenJSI.h
+++ cpp/ShaheenJSI.h
@@ -7,7 +7,9 @@ namespace facebook::react {
 
 class ShaheenJSI {
 public:
-    static jsi::Value executeMwaNative(jsi::Runtime& rt, const jsi::Value& cluster, const jsi::Value& instructionObj);
+    static jsi::Value generateAssociationMwa(jsi::Runtime& rt);
+    static jsi::Value authorizeMwa(jsi::Runtime& rt, const jsi::Value& cluster);
+    static jsi::Value signTransactionsMwa(jsi::Runtime& rt, const jsi::Value& cluster, const jsi::Value& txHex, const jsi::Value& authToken);
 };
 
 } // namespace facebook::react
